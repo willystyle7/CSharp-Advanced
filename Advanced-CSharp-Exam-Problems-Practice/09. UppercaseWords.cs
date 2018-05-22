d302f35119947e817200c000331aafed5cafc682
willystyle7 <ilia111@yahoo.com>
2018-05-22 21:44:03 +0300

    next

---
+++ Advanced-CSharp-Exam-Problems-Practice/09. UppercaseWords.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Security;
+using System.Text.RegularExpressions;
+
+class UppercaseWords
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();       
+        while (input != "END")
+        {
+            string pattern = "(?<=^|[^A-Za-z])([A-Z]+)(?=$|[^A-Za-z])";
+            MatchCollection matches = Regex.Matches(input, pattern);
+            foreach (Match match in matches)
+            {
+                string word = match.Value;
+                string wordToChange = "";
+                if (IsPalindrome(word))
+                {
+                    wordToChange = DoubleWord(word);
+                }
+                else
+                {
+                    wordToChange = ReverseWord(word);
+                }
+                string pattern2 = "(?<=^|[^A-Za-z])(" + word + ")(?=$|[^A-Za-z])";
+                input = Regex.Replace(input, pattern2, wordToChange);
+            }
+            Console.WriteLine(SecurityElement.Escape(input));
+            input = Console.ReadLine();
+        }
+    }
+
+    static bool IsPalindrome(string word)
+    {
+        bool isPalindrome = false;
+        isPalindrome = word == string.Concat(word.ToCharArray().Reverse());
+        return isPalindrome;
+    }
+
+    static string DoubleWord(string word)
+    {
+        string doubleWord = "";
+        for (int i = 0; i < word.Length; i++)
+        {
+            doubleWord += word[i].ToString() + word[i].ToString();
+        }
+        return doubleWord;
+    }
+
+    static string ReverseWord(string word)
+    {
+        string reverseWord = "";
+        for (int i = word.Length - 1; i >= 0; i--)
+        {
+            reverseWord += word[i].ToString();
+        }
+        return reverseWord;
+    }
+
+}
+
---
+++ Advanced-CSharp-Exam-Problems-Practice/15. TheNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+class TheNumbers
+{
+    static void Main()
+    {
+        string message = Console.ReadLine();
+        string pattern = @"\d+";
+        MatchCollection matches = Regex.Matches(message, pattern);        
+       
+        //1 way
+        //List<string> hexNumbers = new List<string>();
+        //foreach (Match match in matches)
+        //{
+        //    int number = int.Parse(match.Value);
+        //    string hexNumber = "0x" + Convert.ToString(number, 16).PadLeft(4, '0').ToUpper();
+        //    hexNumbers.Add(hexNumber);
+        //}
+        
+        //2 way
+        List<string> hexNumbers = (from Match match in matches select String.Format("0x{0}", int.Parse(match.Value).ToString("X4"))).ToList();
+
+        //3 way
+        //List<string> hexNumbers = matches.Cast<Match>().Select(n => String.Format("0x{0}", int.Parse(n.Value).ToString("X4"))).ToList();
+
+        Console.WriteLine(string.Join("-", hexNumbers));
+    }
+}
+
