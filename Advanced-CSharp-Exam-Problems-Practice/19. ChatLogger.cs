c265334bcbed63f8f2f67fb8a52cacabf179c73b
willystyle7 <ilia111@yahoo.com>
2018-05-27 17:06:54 +0300

    2 more

---
+++ Advanced-CSharp-Exam-Problems-Practice/19. ChatLogger.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security;
+
+class ChatLogger
+{
+    static void Main()
+    {
+        string format = "dd-MM-yyyy HH:mm:ss";
+        DateTime now = DateTime.ParseExact(Console.ReadLine(), format, CultureInfo.InvariantCulture);
+        SortedDictionary<DateTime, string> chatLog = new SortedDictionary<DateTime, string>();
+        string input;
+        while ((input = Console.ReadLine()) != "END")
+        {
+            string[] token = input.Split(new[] { " / "}, StringSplitOptions.RemoveEmptyEntries);
+            string message = token[0].Trim();
+            string datetime = token[1].Trim();
+            DateTime Datetime = DateTime.ParseExact(datetime, format, CultureInfo.InvariantCulture);
+            chatLog[Datetime] = message;
+        }
+        foreach (var message in chatLog)
+        {
+            Console.WriteLine("<div>{0}</div>", SecurityElement.Escape(message.Value));
+        }
+        DateTime lastTime = chatLog.Keys.Last();       
+        TimeSpan difference = now - lastTime;        
+        string timestamp = string.Empty;        
+        if (DateTime.Compare(now.Date, lastTime.Date) == 0)
+        {
+            if (difference.TotalHours < 1.0 && difference.TotalMinutes < 1.0)
+            {
+                timestamp = "a few moments ago";
+            }
+            else if (difference.TotalHours < 1.0)
+            {
+                timestamp = (int)difference.TotalMinutes + " minute(s) ago";
+            }
+            else
+            {
+                timestamp = (int)difference.TotalHours + " hour(s) ago";
+
+            }
+        }
+        else if (DateTime.Compare(now.Date, lastTime.Date.AddDays(1.0)) == 0) 
+        {
+            timestamp = "yesterday";
+        }
+        else
+        {
+            timestamp = lastTime.Date.ToString("dd-MM-yyyy");
+        }
+        Console.WriteLine("<p>Last active: <time>{0}</time></p>", timestamp);
+    }
+}
+
---
+++ Advanced-CSharp-Exam-Problems-Practice/20. OMyGirl.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class OMyGirl
+{
+    static void Main()
+    {
+        string key = Console.ReadLine();
+        string pattern = @"";
+        pattern = "(" + Regex.Escape(key[0].ToString());
+        for (int i = 1; i < key.Length - 1; i++)
+        {
+            char currentChar = key[i];
+            if (char.IsDigit(currentChar))
+            {
+                pattern += "[0-9]*";
+            }
+            else if (char.IsLower(currentChar))
+            {
+                pattern += "[a-z]*";
+            }
+            else if (char.IsUpper(currentChar))
+            {
+                pattern += "[A-Z]*";
+            }
+            else
+            {
+                pattern += Regex.Escape(currentChar.ToString());
+            }
+        }
+        pattern += Regex.Escape(key[key.Length - 1].ToString()) + ")";
+        pattern = pattern + "(.{2,6})" + pattern;
+        string input;
+        string result = string.Empty;
+        while ((input = Console.ReadLine()) != "END")
+        {
+            MatchCollection matches = Regex.Matches(input, pattern);
+            foreach (Match match in matches)
+            {
+                result += match.Groups[2].Value;
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
+
